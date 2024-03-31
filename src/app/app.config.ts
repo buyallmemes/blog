665052a0f99d04511900b81b4e20476490fa6058
buyallmemes <165269120+buyallmemes@users.noreply.github.com>
2024-03-31 13:24:17 +0200

    feat: fetching posts from backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,14 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {MarkdownModule} from "ngx-markdown";
+import {provideHttpClient, withFetch} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    importProvidersFrom(MarkdownModule.forRoot())
-  ]
-};
+    providers: [
+      provideRouter(routes),
+      provideAnimationsAsync(),
+      importProvidersFrom(MarkdownModule.forRoot()),
+      provideHttpClient(withFetch())
+    ]
+  }
+;
