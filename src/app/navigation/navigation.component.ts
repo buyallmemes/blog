432d5cde949b4033394f58a05be53916710cd35e
mfenderov <mfenderov@gmail.com>
2024-04-03 19:46:21 +0200

    feat: go home on navigation click

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatIconButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-    imports: [
-        MatIconButton,
-        MatToolbar
-    ],
+  imports: [
+    MatIconButton,
+    MatToolbar,
+    RouterLink
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
-  openLinkInNewWindow(url: string){
+
+  constructor(protected router: Router) {
+  }
+
+  openLinkInNewWindow(url: string) {
     window.open(url, "_blank");
+  }
+
+  goHome() {
+    this.router.navigate(['']);
   }
 }
