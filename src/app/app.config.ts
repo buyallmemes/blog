7c8fa6dfde33583c8705c88a694d9918218edd97
mfenderov <mfenderov@gmail.com>
2024-04-05 22:36:35 +0200

    feat: ssg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,13 @@ import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withFetch} from "@angular/common/http";
 import {HighlightService} from "./blog/highlight.service";
+import {provideClientHydration} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withInMemoryScrolling({anchorScrolling: 'enabled'})),
     provideAnimationsAsync(),
     importProvidersFrom(HighlightService),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()), provideClientHydration()
   ]
 };
