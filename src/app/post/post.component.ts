7c3ac8baf686d01d066d45f101e8f31e3dde0699
mfenderov <mfenderov@gmail.com>
2024-04-17 11:34:46 +0200

    feat: proper share to linkedin functionality with text + copy button

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 import {Post} from "./post";
 import {NgIf} from "@angular/common";
 import {SafeHtmlPipe} from "./safehtmlpipe";
 import {ShareButtonsModule} from "ngx-sharebuttons/buttons";
+import {ShareService} from "ngx-sharebuttons";
 
 @Component({
   selector: 'app-post',
@@ -21,7 +22,35 @@ import {ShareButtonsModule} from "ngx-sharebuttons/buttons";
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export class PostComponent {
-  @Input() post?: Post;
+export class PostComponent implements OnInit {
+  @Input() post!: Post;
 
+  postShareTitle: string = '';
+
+  constructor(private share: ShareService) {
+  }
+
+
+  ngOnInit(): void {
+    this.postShareTitle = 'BuyAllMemes - Blog - ' + this.post.title;
+
+    this.share.addButton('linkedin', LINKEDIN_BUTTON);
+  }
 }
+
+const LINKEDIN_BUTTON = {
+  type: 'linkedin',
+  text: 'LinkedIn',
+  ariaLabel: 'Share on LinkedIn',
+  icon: ['fab', 'linkedin-in'],
+  color: '#006fa6',
+  params: {
+    url: 'url',
+    title: 'title',
+    description: 'text'
+  },
+  share: {
+    desktop: 'https://www.linkedin.com/shareArticle?'
+  }
+};
+
